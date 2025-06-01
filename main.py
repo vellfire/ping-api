6995a5dcd3e2bc408f0181d18544b467a37dd4f2
vellfire <will@mcda.dev>
2025-06-02 00:19:58 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import subprocess
+
+app = FastAPI()
+
+host = "172.16.110.2"
+
+@app.get("/status")
+def ping():
+    command = ['ping', '-c', '1', '-W', '1', host]
+
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    return{"online": result.returncode == 0}
